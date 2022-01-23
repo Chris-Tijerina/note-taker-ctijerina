e949d7470ce96c917fac8b2f0b9bbfd51d2b5c11
Christopher <ctijerina@live.com>
2022-01-23 14:11:09 -0600

    working post route created, still unsure about get route, needs more work

--- server.js
+++ server.js
@@ -2,24 +2,41 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
+const { notes } = require("./db/db");
 
 // parse incoming string or array data
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-const { db } = require("./db/db.json");
+app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-  });
-
-app.get("/api/db", (req, res) => {
-    res.send("hello");
 });
 
-app.listen(PORT, () => {
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get("/api/notes", (req, res) => {
+    // Use fs.readFile() method to read the file
+    fs.readFile('./db/db.json', 'utf8', function (err, data) {
+        // Display the file content
+        console.log(data);
+    });
+});
+
+app.post("/api/notes", (req, res) => {
+    console.log(req.body);
+})
+
+    app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
 
