a12f19d037a2af0c0895ac5fc004a28e2f0d6004
Christopher <ctijerina@live.com>
2022-01-23 14:28:06 -0600

    finally got the post and get route sorted, time to fill them in with their reading and writing of files

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
 const { notes } = require("./db/db");
+const fs = require('fs');
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +12,22 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+app.get("/api/notes", (req, res) => {
+    // Use fs.readFile() method to read the file
+    fs.readFile('./db/db.json', 'utf8', function (err, data) {
+        // Display the file content
+        console.log(data);
+    });
+    console.log('readFile called');
+});
+
+app.post("/api/notes", (req, res) => {
+    console.log(req.body);
+})
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
@@ -20,23 +37,9 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get("/api/notes", (req, res) => {
-    // Use fs.readFile() method to read the file
-    fs.readFile('./db/db.json', 'utf8', function (err, data) {
-        // Display the file content
-        console.log(data);
-    });
-});
-
-app.post("/api/notes", (req, res) => {
-    console.log(req.body);
-})
-
-    app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
 });
 
