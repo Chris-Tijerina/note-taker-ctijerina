f3b25209447fe8402f5b1af1dd1dadc29482d51f
Christopher <ctijerina@live.com>
2022-01-22 19:54:21 -0600

    restructed folders due to pathing issue

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+// parse incoming JSON data
+app.use(express.json());
+
+const { db } = require("./db/db.json");
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get("/api/db", (req, res) => {
+    res.json();
+});
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
+
